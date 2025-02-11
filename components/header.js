cf333494df487fe58509bd2ae734bcf11f999788
sumukhj1219 <sumukhjoshi75@gmail.com>
2025-02-11 18:12:04 +0530

    79 % seo score

--- components/header.js
+++ components/header.js
@@ -3,26 +3,27 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="w-full  h-[100vh] relative">
-      <img 
-        src='/sydney.jpg'
-        className='w-full h-full object-cover'
+    <div className="w-full h-[100vh] relative">
+      <Image 
+        src="/sydney.jpg"
+        layout="fill"
+        objectFit="cover"
         alt="Sydney"
+        priority
       />
-      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-tl from-black via-transparent to-transparent opacity-50"></div>
-      <div className="absolute top-0 right-0 w-full h-full bg-gradient-to-tr from-black via-transparent to-transparent opacity-50"></div>
-      <div className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-bl from-black via-transparent to-transparent opacity-50"></div>
-      <div className="absolute bottom-0 right-0 w-full h-full bg-gradient-to-br from-black via-transparent to-transparent opacity-50"></div>
+
+      <div className="absolute inset-0 bg-gradient-to-tl from-black via-transparent to-transparent opacity-50"></div>
+      <div className="absolute inset-0 bg-gradient-to-tr from-black via-transparent to-transparent opacity-50"></div>
+      <div className="absolute inset-0 bg-gradient-to-bl from-black via-transparent to-transparent opacity-50"></div>
+      <div className="absolute inset-0 bg-gradient-to-br from-black via-transparent to-transparent opacity-50"></div>
 
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center px-6 sm:px-8">
-        <span className="text-3xl sm:text-4xl lg:text-5xl font-bold text-white text-shadow-md">
-        Your one-stop solution for all your lending needs
-        </span>
-        <div className="mt-4">
-          <span className="text-xl sm:text-2xl font-light text-white opacity-90">
-            Tailored solutions to help you achieve homeownership and financial success.
-          </span>
-        </div>
+        <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-white text-shadow-md">
+          Your One-Stop Solution for All Your Lending Needs
+        </h1>
+        <p className="mt-4 text-xl sm:text-2xl font-light text-white opacity-90">
+          Tailored solutions to help you achieve homeownership and financial success.
+        </p>
       </div>
     </div>
   );
